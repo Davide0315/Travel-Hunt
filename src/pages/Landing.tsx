b32cc13b3f69448bb056285d317770f34d83b586
Davide Romano <165106712+Davide0315@users.noreply.github.com>
2025-05-06 22:46:38 +0200

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -121,7 +121,7 @@ const Landing = () => {
             {/* Missions grid */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {missions.slice(0, 1).map(mission => (
-                <div key={mission.id}>
+                <div key={mission.id}  className="md:col-span-2 lg:col-span-3">
                   <MissionCard mission={mission} variant="featured" />
                 </div>
               ))}
